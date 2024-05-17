a524285064deee7fee04ef4d9b899ae8bb8beddb
NewEra039 <power0f.new.era@gmail.com>
2024-05-17 19:40:09 +0200

    first commit

---
+++ app/src/main/java/com/example/loginregister/LoginRegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.loginregister;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.calculator0ffood.R;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+    }
+}
