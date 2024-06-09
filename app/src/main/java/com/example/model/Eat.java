70f09a7eea7c1fdfa74eeb7f193de61426348c94
NewEra039 <power0f.new.era@gmail.com>
2024-06-09 21:41:19 +0200

    For

--- app/src/main/java/com/example/model/Eat.java
+++ app/src/main/java/com/example/model/Eat.java
@@ -7,32 +7,48 @@ public class Eat {
     private double carbs;
     private double calories;
 
+    public Eat(String name, double protein, double fat, double carbs, double calories) {
+        this.name = name;
+        this.protein = protein;
+        this.fat = fat;
+        this.carbs = carbs;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getProtein() {
         return protein;
-    }
-
-    public double getFat() {
-        return fat;
-    }
-
-    public double getCarbs() {
-        return carbs;
-    }
-
-    public double getCalories() {
-        return calories;
     }
 
     public void setProtein(double protein) {
         this.protein = protein;
     }
 
+    public double getFat() {
+        return fat;
+    }
+
     public void setFat(double fat) {
         this.fat = fat;
     }
 
+    public double getCarbs() {
+        return carbs;
+    }
+
     public void setCarbs(double carbs) {
         this.carbs = carbs;
+    }
+
+    public double getCalories() {
+        return calories;
     }
 
     public void setCalories(double calories) {
